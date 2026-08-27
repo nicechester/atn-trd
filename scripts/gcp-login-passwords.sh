efa8ddfe938c5ad2a74fcaa1c62503864691be13
Chester Kim <violkim@gmail.com>
2026-08-27 08:16:54 -0700

    chore: remove GCP deployment scripts
    Cloud Run approach abandoned. Moving to Compute Engine with local SQLite.
    
    Refs #118

--- scripts/gcp-login-passwords.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Enter chester password:" && read -s CHESTER_PW && echo && \
-echo "Enter guest password:" && read -s GUEST_PW && echo && \
-echo -n "$CHESTER_PW" | gcloud secrets create AUTH_PASSWORD_CHESTER --data-file=- 2>/dev/null || \
-echo -n "$CHESTER_PW" | gcloud secrets versions add AUTH_PASSWORD_CHESTER --data-file=- && \
-echo -n "$GUEST_PW" | gcloud secrets create AUTH_PASSWORD_GUEST --data-file=- 2>/dev/null || \
-echo -n "$GUEST_PW" | gcloud secrets versions add AUTH_PASSWORD_GUEST --data-file=-
