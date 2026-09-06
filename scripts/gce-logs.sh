9f133fd1c87cfc1562a9a0f036a888300b64d424
Chester Kim <violkim@gmail.com>
2026-09-06 08:03:51 -0700

    chore: update gce scripts for native deployment (no docker)

--- scripts/gce-logs.sh
+++ scripts/gce-logs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# View atn-trd container logs from local
+# View atn-trd service logs from local
 
-gcloud compute ssh atn-trd-vm --zone=us-central1-a --project=autonomous-trader-506715 --command="docker logs atn-trd ${*:---tail=100}"
+gcloud compute ssh atn-trd-vm --zone=us-central1-a --project=autonomous-trader-506715 \
+  --command="sudo journalctl -u atn-trd ${*:---tail=100 --no-pager}"
